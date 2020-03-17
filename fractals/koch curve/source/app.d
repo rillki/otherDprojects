691ba66c83e69d309f9c78c7eb7f9dad75048783
Ki Rill <rillk500@gmail.com>
2020-03-17 16:53:59 +0600

    Update app.d

--- fractals/koch curve/source/app.d
+++ fractals/koch curve/source/app.d
@@ -3,7 +3,7 @@ import std.math: sin, cos, PI, sqrt, atan2;
 import raylib;
 
 void main() {
-	immutable int screenWidth = 640;
+    immutable int screenWidth = 640;
     immutable int screenHeight = 480;
 
     InitWindow(screenWidth, screenHeight, "Dlang Koch Curve");
@@ -16,20 +16,20 @@ void main() {
 
     int limit = 0;
     while (!WindowShouldClose()) {
-		// process events
+	// process events
 
-		// update
+	// update
 
-		// draw
-		BeginDrawing();
+	// draw
+	BeginDrawing();
         ClearBackground(RAYWHITE);
 
-		kochCurve(Vector2(10, screenHeight/2), Vector2(screenWidth-10, screenHeight/2), 6);
+	kochCurve(Vector2(10, screenHeight/2), Vector2(screenWidth-10, screenHeight/2), 6);
 
-		// Koch Snowflake
-		/*kochCurve(p0, p1, 6);
-		kochCurve(p1, p2, 6);
-		kochCurve(p2, p0, 6);*/
+	// Koch Snowflake
+	/*kochCurve(p0, p1, 6);
+	kochCurve(p1, p2, 6);
+	kochCurve(p2, p0, 6);*/
 
         EndDrawing();
     }
@@ -46,10 +46,10 @@ void kochCurveAnimation(Vector2 p0, Vector2 p1, ref int limit) {
 }
 
 void kochCurve(Vector2 p0, Vector2 p1, int limit) {
-	Vector2 dist = Vector2(p1.x - p0.x, p1.y - p0.y);		// distance between the points p0 and p1
+	Vector2 dist = Vector2(p1.x - p0.x, p1.y - p0.y);	// distance between the points p0 and p1
 
 	float length = sqrt(dist.x*dist.x + dist.y*dist.y)/3;	// length from p0 to p1 => a^2 + b^2 = c^2
-	float angle = atan2(dist.y, dist.x) - PI/3; 			// = 60*PI/180  |  change the PI/3 to PI/1.6 for wave like pattern
+	float angle = atan2(dist.y, dist.x) - PI/3; 		// = 60*PI/180  |  change the PI/3 to PI/1.6 for wave like pattern
 
 	Vector2 a = Vector2(p0.x + dist.x/3, p0.y + dist.y/3);
 	Vector2 c = Vector2(p1.x - dist.x/3, p1.y - dist.y/3);
